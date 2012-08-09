f4666d607d2ff311018826aa9236155bc1a9abf2
Neeraj Kumar <neerajpkumar@gmail.com>
2012-08-10 04:40:25 +0530

    Adding code for calendar aside for octopress bloggin framework

--- README.md
+++ README.md
@@ -2,3 +2,25 @@ octopress-calendar-aside
 ========================
 
 Show a calendar(as aside) with clickable dates to your posts in octopress.
+
+This plugin creates a calendar, which can be propogated by month and the dates automatically link to the posts you have created.
+So lets say your posts were created on 11th August, 25th August, 13th July etc. the dates on the calendar are aotomatically linked to your posts on those days.
+All this without ANY configuration.
+
+All you need to do is:
+a. add the file calendar.html to the default asides location. (.themes/classic/source/_includes/asides)
+b. add the file calendar.js to the default javascript location. (.themes/classic/source/javascript)
+c. add the calendar aside to _config.yml file. (see the config.yml in commited source)
+d. add the images prev.png and next.png to .themes/classic/source/images/. (these images are free for non-commercial purposes)
+
+
+And voila, just do a rake generate and push your blog code, you should see the calendar aside up and running on your blog.
+
+----
+About the author:
+
+Neeraj Kumar is an Engineer working for the Gmail team at Google,India.
+A coffee/tea addict, he loves reading about HCI techniques and you would find his new HTML5 baked blog at http://titansturf.in
+
+He also likes to work on creating mobile applications, and has had a few stints with phonegap and native android development.
+
---
+++ calendar.js
@@ -0,0 +1,166 @@
+// these are labels for the days of the week
+cal_days_labels = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
+
+// these are human-readable month name labels, in order
+cal_months_labels = ['January', 'February', 'March', 'April',
+                     'May', 'June', 'July', 'August', 'September',
+                     'October', 'November', 'December'];
+
+// these are the days of the week for each month, in order
+cal_days_in_month = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+
+// this is the current date
+cal_current_date = new Date(); 
+
+function generateGuid()
+{
+  var result, i, j;
+  result = '';
+  for(j=0; j<32; j++) {
+    if( j == 8 || j == 12|| j == 16|| j == 20)
+    result = result + '-';
+    i = Math.floor(Math.random()*16).toString(16).toUpperCase();
+    result = result + i;
+  }
+  return result;
+}
+
+function createCalendar(id)
+{
+	
+}
+
+function Calendar(month, year, highlightArray, highlightLinks) {
+	this.month = (isNaN(month) || month == null) ? cal_current_date.getMonth() : month;
+	this.year  = (isNaN(year) || year == null) ? cal_current_date.getFullYear() : year;
+	this.html = '';
+	this.guid = '';
+	//array of dates which are to be highlighted.
+	//Expects the dates in the form MM/DD/YYYY
+	this.highlightArray = highlightArray;
+	this.highlightLinks = highlightLinks;
+}
+
+Calendar.prototype.generateHTML = function(id) {
+  if(id == undefined)
+  {
+    id = generateGuid();
+  }
+  this.guid = id
+  
+  // get first day of month
+  var firstDay = new Date(this.year, this.month, 1);
+  var startingDay = firstDay.getDay();
+  
+  // find number of days in month
+  var monthLength = cal_days_in_month[this.month];
+  
+  // compensate for leap year
+  if (this.month == 1) { // February only!
+    if((this.year % 4 == 0 && this.year % 100 != 0) || this.year % 400 == 0){
+      monthLength = 29;
+    }
+  }
+  
+  // do the header
+  var monthName = cal_months_labels[this.month]
+  var prevYear;
+  var nextYear;
+  var prevMonth;
+  var nextMonth;
+  if(this.month == 0)
+  {
+	prevYear = this.year -1;
+	nextYear = this.year;
+	prevMonth = 11;
+	nextMonth = this.month +1;
+  }
+  else if(this.month == 11)
+  {
+	prevYear = this.year;
+	nextYear = this.year+1;
+	prevMonth = this.month -1;
+	nextMonth = 1;
+  }
+  else
+  {
+	prevYear = this.year;
+	nextYear = this.year;
+	prevMonth = this.month - 1;
+	nextMonth = this.month + 1;
+  }
+  var html = '<table id="'+id+'-calendar-table" class="calendar-table">';
+  html += '<tr><th colspan="7">';
+  html += '<a id="'+this.id+'-prev-calendar-link" class="prev-link" style="height:25px;width:25px" href="#" onclick="monthCalendar(\''+this.guid+'\','+prevYear+','+prevMonth+');return false;" ><img class="prevCalendarLink" style="height:25px;width:25px;border:none 0px;padding-top:2px" src="/images/prev.png"></img></a>';
+  html +=  monthName + "&nbsp;" + this.year;
+  html += '<a id="'+this.id+'-next-calendar-link" class="next-link" style="height:25px;width:25px" href="#" onclick="monthCalendar(\''+this.guid+'\','+nextYear+','+nextMonth+');return false;"><img class="nextCalendarLink"  style="height:25px;width:25px;border:none 0px;padding-top:2px" src="/images/next.png"></img></a>';
+  html += '</th></tr>';
+  html += '<tr class="calendar-header">';
+  for(var i = 0; i <= 6; i++ ){
+    html += '<td class="calendar-header-day">';
+    html += cal_days_labels[i];
+    html += '</td>';
+  }
+  html += '</tr><tr>';
+
+  // fill in the days
+  var day = 1;
+  // this loop is for is weeks (rows)
+  for (var i = 0; i < 9; i++) {
+    // this loop is for weekdays (cells)
+    for (var j = 0; j <= 6; j++) {
+		var strMonth;
+		var strDay;
+	  if(day < 10 )
+	  {
+		strDay = '0'+day;
+	  }
+	  else
+	  {
+		strDay = day;
+	  }
+	  if( (this.month+1) < 10 )
+	  {
+		strMonth = '0' + (this.month +1);
+		
+	  }
+	  else
+	  {
+		strMonth = this.month+1;
+	  }
+	  var date = strMonth+"/"+strDay+"/"+this.year;
+	  var isEvent =  this.highlightArray.indexOf(date) > -1;
+	  if(isEvent){
+		html += '<td><span class="highlighted-calendar-day"><a href="'+this.highlightLinks[this.highlightArray.indexOf(date)]+'">';
+	  }else{
+		html += '<td><span class="calendar-day">'; 
+	  }
+	  
+      if (day <= monthLength && (i > 0 || j >= startingDay)) {
+        html += day;
+        day++;
+      }
+	  if(isEvent)
+		html += '</a></span></td>';
+	  else
+		html += '</span></td>';
+	  
+    }
+    // stop making rows if we've run out of days
+    if (day > monthLength) {
+      break;
+    } else {
+      html += '</tr><tr>';
+    }
+  }
+  html += '</tr></table>';
+  
+  
+  this.html = html;
+}
+
+Calendar.prototype.getHTML = function() {
+  return this.html;
+}
+
+
